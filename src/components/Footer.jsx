b6758d08865459b8561fcb60d1f0d4fc6c910c12
Jasmine Henry <Jasminehenry356@gmail.com>
2023-05-01 07:21:51 -0400

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer () {
+    const githubURL = 'https://github.com/jas356/counter-app-jch2'
+      return (
+        <footer>
+          <p><small>&copy; 2023</small><br />
+          <a href={githubURL} target="_blank" rel="noreferrer">See Code in Github</a></p>
+        </footer>
+      )
+    }
